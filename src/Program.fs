4f0eaeadbadb8e12058e0fb82217c95d2a5a333e
Rajath Ganesh <rajath2774@gmail.com>
2020-09-04 18:06:45 -0400

    First Async F# program

---
+++ src/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let asyncFunc () = 
+    async {
+        do! Async.Sleep 5000
+        printf "Inside Async:"
+        return 42
+    }
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let x = asyncFunc() |> Async.RunSynchronously
+    printf "%i" x
+    0 // return an integer exit code
